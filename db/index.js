677c10e755a50ba8994f06b35a37f88fc0d9b91a
Rahi Modi <45651940+RuhiModi@users.noreply.github.com>
2026-02-03 12:39:30 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,16 +1,9 @@
+// db/index.js
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // REQUIRED for Render
+  ssl: { rejectUnauthorized: false }
 });
 
-export async function query(text, params) {
-  const client = await pool.connect();
-  try {
-    return await client.query(text, params);
-  } finally {
-    client.release();
-  }
-}
